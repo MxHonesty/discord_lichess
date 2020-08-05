26f8d58d16c5402644ae705d8e7c4bd3aead73db
Stelian <stoianstelian33@gmail.com>
2020-08-05 23:30:37 +0300

    classical rating

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from li import getBlitzRating
+from li import getBlitzRating, getClassicRating
 
 client = discord.Client()
 
@@ -12,11 +12,18 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$rating'):
+    if message.content.startswith('$blitz'):
         nume = message.content[8:]
         try:
             val = getBlitzRating(nume)
-            await message.channel.send('Ratingul lui ' + nume + ' este ' +  str(val))
+            await message.channel.send('Ratingul blitza al lui ' + nume + ' este ' +  str(val))
+        except:
+            await message.channel.send('Nume invalid')
+    elif message.content.startswith('$classic'):
+        nume = message.content[9:]
+        try:
+            val = getClassicRating(nume)
+            await message.channel.send('Ratingul classical al lui ' + nume + ' este ' + str(val))
         except:
             await message.channel.send('Nume invalid')
 
--- li.py
+++ li.py
@@ -6,11 +6,23 @@ def getBlitzRating(name):
     user = lichess.api.user(name)
     return(user['perfs']['blitz']['rating'])
 
+def getClassicRating(name):
+    user = lichess.api.user(name)
+    return(user['perfs']['classical']['rating'])
+
+
 if __name__ == '__main__':
     if(len(sys.argv) > 1 ):
-        if(sys.argv[1] == "user"):
+        if(sys.argv[1] == "blitz"):
             try:
                 print(sys.argv[2])
                 print(getBlitzRating(sys.argv[2]))
+            except:
+                print("User not found")
+
+        if(sys.argv[1] == "classic"):
+            try:
+                print(sys.argv[2])
+                print(getClassicRating(sys.argv[2]))
             except:
                 print("User not found")
