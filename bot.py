a46e3c03db58cf344434ad558d723e7259aa7faf
Stelian <stoianstelian33@gmail.com>
2020-08-06 21:18:07 +0300

    rapid and bullet chess

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import discord
-from li import getBlitzRating, getClassicRating
+from li import getBlitzRating, getClassicRating, getRapidRating, getBulletRating
 
 client = discord.Client()
 
@@ -12,14 +12,15 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$blitz'):
+    if message.content.startswith('$blitz'):        #blitz
         nume = message.content[8:]
         try:
             val = getBlitzRating(nume)
             await message.channel.send('Ratingul blitza al lui ' + nume + ' este ' +  str(val))
         except:
             await message.channel.send('Nume invalid')
-    elif message.content.startswith('$classic'):
+
+    elif message.content.startswith('$classic'):    #classical
         nume = message.content[9:]
         try:
             val = getClassicRating(nume)
@@ -27,4 +28,20 @@ async def on_message(message):
         except:
             await message.channel.send('Nume invalid')
 
-client.run(TOKEN)
+    elif message.content.startswith('$rapid'):      #rapid
+        nume = message.content[7:]
+        try:
+            val = getRapidRating(nume)
+            await message.channel.send('Ratiungul rapid al lui ' + nume + ' este ' + str(val))
+        except:
+            await message.channel.send('Nume invalid')
+
+    elif message.content.startswith('$bullet'): #bullet
+        nume = message.content[8:]
+        try:
+            val = getBulletRating(nume)
+            await message.channel.send('Ratiungul bullet al lui ' + nume + ' este ' + str(val))
+        except:
+            await message.channel.send('Nume invalid')
+
+client.run('NzM5OTYwNjI4NTU4NTYxMzM0.XyiECQ._NE5jDfQeNJcK5OiSQMlZQfNY0s')
--- li.py
+++ li.py
@@ -10,6 +10,13 @@ def getClassicRating(name):
     user = lichess.api.user(name)
     return(user['perfs']['classical']['rating'])
 
+def getRapidRating(name):
+    user = lichess.api.user(name)
+    return(user['perfs']['rapid']['rating'])
+
+def getBulletRating(name):
+    user = lichess.api.user(name)
+    return(user['perfs']['bullet']['rating'])
 
 if __name__ == '__main__':
     if(len(sys.argv) > 1 ):
@@ -26,3 +33,17 @@ if __name__ == '__main__':
                 print(getClassicRating(sys.argv[2]))
             except:
                 print("User not found")
+
+        if(sys.argv[1] == "rapid"):
+           try:
+               print(sys.argv[2])
+               print(getRapidRating(sys.argv[2]))
+           except:
+               print("User not found")
+
+        if(sys.argv[1] == "bullet"):
+           try:
+               print(sys.argv[2])
+               print(getBulletRating(sys.argv[2]))
+           except:
+               print("User not found")
